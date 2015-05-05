5a48b35c36214f9fe1c8a98007a7a2ef9df45e8b
Stein Jin <stein.jin@gmail.com>
2015-05-05 22:28:52 +0800

    dasss21

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -27,7 +27,7 @@ Rails.application.configure do
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = true
+  config.assets.compile = false
 
   # Generate digests for assets URLs.
   config.assets.digest = true
@@ -75,7 +75,5 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
-  config.serve_static_assets = true
-config.assets.precompile += %w(*.png *.jpg *.jpeg *.gif)
-config.assets.compile = false # we don't want compilation fallbacks
+
 end
