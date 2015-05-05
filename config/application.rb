6484049f436de16bdc84d05f85c020991d65d06e
Stein Jin <stein.jin@gmail.com>
2015-05-05 22:08:36 +0800

    aaaaadsdsf

--- config/application.rb
+++ config/application.rb
@@ -5,7 +5,9 @@ require 'rails/all'
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
-
+config.assets.precompile += Ckeditor.assets
+config.assets.precompile += %w( ckeditor/* )
+config.autoload_paths += %W(#{config.root}/app/models/ckeditor)
 module Treebook
   class Application < Rails::Application
     # Settings in config/environments/* take precedence over those specified here.
